fb31fdf8830b47f250329b6cfd64ff7f93ce9a5b
xiongchengfu <xiongchengfu@visioninsight.com>
2021-06-29 21:09:39 +0800

    <opt> 697.数组的度

--- week2/697.数组的度.swift
+++ week2/697.数组的度.swift
@@ -7,52 +7,39 @@
 // @lc code=start
 class Solution {
     func findShortestSubArray(_ nums: [Int]) -> Int {
-        /// 存储key=数组元素；value=数组元素出现次数
-        var du = [Int:Int]()
-        /// 存储key=数组元素；value=数组元素出现的坐标集
-        var indexArray = [Int:[Int]]()
+        ///存储key=数组元素；
+        ///value=数组[元素出现的次数，元素第一次出现坐标，元素最后一次出现坐标]
+        var du = [Int:[Int]]()
 
         for (index, item) in nums.enumerated() {
-            /// 得到数组元素对应的次数map
-            if let count = du[item] {
-                du[item]! = count + 1
+            /// 得到数组元素对应的次数及坐标
+            if let array = du[item] {
+                var temp = array
+                temp[0] += 1
+                temp[2] = index
+                du[item] = temp
             } else {
-                du[item] = 1
-            }
-            /// 得到数组元素对应的坐标集合map，坐标数组有序
-            if let indexs = indexArray[item] {
-                var temp = indexs
-                temp.append(index)
-                indexArray[item] = temp
-            } else {
-                indexArray[item] = [index]
+                du[item] = [1, index, index]
             }
         }
         print(du)
-        print(indexArray)
         
-        /// 得到是最大度的元素的集合
-        var maxArray = [Int]()
-        var max = 0
-        for (key, value) in du {
-            if (value >= max) {
-                if (value > max) {
-                    maxArray.removeAll()
+        var maxDu = 0
+        var minLen = 0
+
+        for (index, item) in du {
+            let arr = item
+            if maxDu < arr[0] {
+                maxDu = arr[0]
+                minLen = arr[2] - arr[1] + 1;
+            } else if (maxDu == arr[0]) {
+                if (minLen > arr[2] - arr[1] + 1) {
+                    minLen = arr[2] - arr[1] + 1;
                 }
-                maxArray.append(key)
-                max = value
             }
         }
-        print(maxArray)
-        print(max)
-        /// 遍历最大度元素的集合，算出集合中[坐标结束 - 坐标开始]最小的值
-        var ans = 50000
-        for item in maxArray {
-            if let indexs = indexArray[item] {
-                ans = min(ans, indexs[indexs.count-1] - indexs[0]) 
-            }
-        }
-        return ans + 1
+
+        return minLen
     }
 }
 // @lc code=end
