624c3825aaeed0c846bcc977bc88404114bb5b33
xiongchengfu <xiongchengfu@visioninsight.com>
2021-08-10 23:03:11 +0800

    <opt> 771.宝石与石头

--- week8/771.宝石与石头.swift
+++ week8/771.宝石与石头.swift
@@ -5,15 +5,47 @@
  */
 
 // @lc code=start
+public struct HashSet<T: Hashable> {
+    fileprivate var dictionary = Dictionary<T, Bool>()
+
+    public init() {
+
+    }
+
+    public mutating func insert(_ element: T) {
+        dictionary[element] = true
+    }
+
+    public mutating func remove(_ element: T) {
+        dictionary[element] = nil
+    }
+
+    public func contains(_ element: T) -> Bool {
+        return dictionary[element] != nil
+    }
+
+    public func allElements() -> [T] {
+        return Array(dictionary.keys)
+    }
+
+    public var count: Int {
+        return dictionary.count
+    }
+
+    public var isEmpty: Bool {
+        return dictionary.isEmpty
+    }
+}
+
 class Solution {
     func numJewelsInStones(_ jewels: String, _ stones: String) -> Int {
-        var jewelDic = [Character : Int]()
+        var jewelDic = HashSet<Character>()
         for j in jewels {
-            jewelDic[j] = 1
+            jewelDic.insert(j)
         }
         var res = 0
         for s in stones {
-            if let _ = jewelDic[s] {
+            if (jewelDic.contains(s)) {
                 res += 1
             }
         }
